56cb2b1e756587c18bb9cfcb772e7f083a26974a
darwish791 <darwish791@gmail.com>
2018-12-09 19:15:31 +0800

    Progress

---
+++ PSM/Assets/sceneHentiRokok.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneHentiRokok : MonoBehaviour {
+
+    public void kenapaPerluBerhenti()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ PSM/Assets/sceneMainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneMainMenu : MonoBehaviour {
+
+    public void ModulHentiRokok()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+		
+	
+}
+//"Modul Henti Rokok", LoadSceneMode.Single
